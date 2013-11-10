84e04a2c4e0cd65ed34831fda0a95608e9771f00
Alex Burck <burck1@illinois.edu>
2013-11-10 06:58:34 -0600

    message passing and gps work perfectly

--- gps_arduino/gps_arduino.ino
+++ gps_arduino/gps_arduino.ino
@@ -1,14 +1,17 @@
-//#include <TinyGPS++.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <EasyTransferI2C.h>
 
-// stty -F /dev/ttyACM0 speed 1200 cs8 -cstopb -parenb
-
-#define I2C_ADDRESS_MASTER 0x1
 #define I2C_ADDRESS_SLAVE 0x2
 
-//TinyGPSPlus gps;
+static const int TXPin = 3, RXPin = 4;
+static const unsigned long GPSBaud = 9600;
+
+TinyGPSPlus gps;
 EasyTransferI2C ET;
+
+SoftwareSerial ss(RXPin, TXPin);
 
 struct SEND_DATA_STRUCTURE {
   float my_lat;
@@ -20,98 +23,55 @@ struct SEND_DATA_STRUCTURE {
 SEND_DATA_STRUCTURE mydata;
 
 void setup() {
-  // Used to communicate with GPS Module
-//  Serial1.begin(9600);
   Serial.begin(9600);
-
-  mydata.my_lat = 0.0;
-  mydata.my_lng = 0.0;
-  mydata.my_date = 0;
-  mydata.my_time = 0;
-
-  // Used to communicate with other Arduino as slave sender
-  Wire.begin(I2C_ADDRESS_SLAVE);
-  Wire.onRequest(requestEvent);
   
+  // Used to communicate with GPS Module
+  ss.begin(GPSBaud);
+
+  mydata.my_lat = 40.114119;
+  mydata.my_lng = -88.225134;
+  mydata.my_date = 11111;
+  mydata.my_time = 11111;
+
+  Wire.begin();
   ET.begin(details(mydata), &Wire);
+  
+  // Use LED for debugging
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-//  while (Serial1.available() > 0) {
-//    gps.encode(Serial1.read());
-//  }
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
+  }
   
-//  if (gps.location.isUpdated()) {
-//    mydata.my_lat = gps.location.lat();
-//    mydata.my_lng = gps.location.lng();
-//    mydata.my_date = gps.date.value();
-//    mydata.my_time = gps.time.value();
-//  }
-  mydata.my_lat = 40.114119;
-  mydata.my_lng = -88.225134;
-  mydata.my_date = 110913;
-  mydata.my_time = 03142731;
+  if (gps.location.isUpdated()) {
+    if (gps.location.isValid()) {
+      mydata.my_lat = gps.location.lat();
+      mydata.my_lng = gps.location.lng();
+    }
+    if (gps.date.isValid()) {
+      mydata.my_date = gps.date.value();
+    }
+    if (gps.time.isValid()) {
+      mydata.my_time = gps.time.value();
+    }
+  }
   
+  Serial.print(mydata.my_lat,6);
+  Serial.print(",");
+  Serial.print(mydata.my_lng,6);
+  Serial.print(",");
+  Serial.print(mydata.my_date);
+  Serial.print(",");
+  Serial.println(mydata.my_time);
+  
+  ET.sendData(I2C_ADDRESS_SLAVE);
+  
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+
+  // Send every 2 seconds
+  delay(2 * 1000);
 }
-
-// function that executes whenever data is requested by master
-void requestEvent() {
-  Wire.write('A');
-//  ET.sendData(I2C_ADDRESS_MASTER);
-}
-
-//void requestEvent() {
-//  Wire.beginTransmission(I2C_ADDRESS_MASTER);
-//  if (gps.location.isUpdated()) {
-//    I2C_writeAnything(gps.location.lat()); // double float
-//    I2C_writeAnything(gps.location.lng()); // double float
-//    I2C_writeAnything(gps.date.value()); // u32
-//    I2C_writeAnything(gps.time.value()); // u32
-//  }
-//  else {
-//    float x = 0.0;
-//    unsigned int y = 0;
-//    I2C_writeAnything(x); // double float
-//    I2C_writeAnything(x); // double float
-//    I2C_writeAnything(y); // u32
-//    I2C_writeAnything(y); // u32
-//  }
-//  if (Wire.endTransmission () != 0) {
-//      Serial.println("I2C Transmission Failed");
-//  }
-  
-//  if (gps.location.isUpdated()) {
-//    char *lat = float_to_string(gps.location.lat());
-//    char *lng = float_to_string(gps.location.lng());
-//    Wire.write(lat);
-//    Wire.write(lng);
-//    Wire.write(gps.date.value()); // u32 = 4bytes
-//    Wire.write(gps.time.value()); // u32 = 4bytes
-//  }
-//  else {
-//    Wire.write(0);
-//  }
-//}
-
-//char* float_to_string(double x) {
-//  char fin[12];
-//  
-//  if (x >= 0) { fin[0] = '+'; }
-//  else { fin[0] = '-'; x = -1*x; }
-//  
-//  unsigned int i = floor(x);
-//  fin[3] = '0' + (i % 10); i /= 10;
-//  fin[2] = '0' + (i % 10); i /= 10;
-//  fin[1] = '0' + (i % 10); i /= 10;
-//  
-//  fin[4] = '.';
-//  
-//  for (i=5; i < 11; i++) {
-//    x *= 10;
-//    unsigned int j = floor(x);
-//    fin[i] = '0' + (j%10);
-//  }
-//  
-//  fin[11] = 0;
-//  return fin;
-//}
--- gsm_arduino/gsm_arduino.ino
+++ gsm_arduino/gsm_arduino.ino
@@ -1,7 +1,35 @@
 #include <Wire.h>
 #include <EasyTransferI2C.h>
+#include <GSM.h>
 
-#define I2C_ADDRESS_MASTER 0x1
+/* ---------- Xively Setup ---------- */
+#define APIKEY     "9DGhzPeqvXxae8repC5PD2159hZZrn6IlNqpIYpsdt8ZS7jQ"
+#define FEEDID     565709094
+#define USERAGENT  "Car Tracker" // project name
+
+char server[] = "api.xively.com";
+char path[] = "/v2/feeds";
+
+unsigned long lastConnectionTime = 0; // milliseconds
+boolean lastConnected = false; // state of the connection last time through the main loop
+const unsigned long postingInterval = 20*1000; //delay between updates to Xively.com
+
+
+/* ---------- GSM Setup ---------- */
+#define PINNUMBER      ""
+#define GPRS_APN       "bluevia.movistar.es"
+#define GPRS_LOGIN     ""
+#define GPRS_PASSWORD  ""
+
+GSMClient client;
+GPRS gprs;
+GSM gsmAccess;
+
+boolean sent_get_request = false;
+boolean recieved_all_data = false;
+
+
+/* ---------- I2C Setup ---------- */
 #define I2C_ADDRESS_SLAVE 0x2
 
 EasyTransferI2C ET;
@@ -12,42 +40,175 @@ struct RECEIVE_DATA_STRUCTURE {
   unsigned int my_date;
   unsigned int my_time;
 };
-
 RECEIVE_DATA_STRUCTURE mydata;
 
-//volatile float lat;
-//volatile float lng;
-//volatile unsigned int date;
-//volatile unsigned int time;
 
 void setup()
 {
   Serial.begin(9600);
   
-  Wire.begin(I2C_ADDRESS_MASTER);
+  Wire.begin(I2C_ADDRESS_SLAVE);
   ET.begin(details(mydata), &Wire);
+  
+  //define handler function on receiving data
+  Wire.onReceive(receive);
+  
+  // Use LED for debugging
+  pinMode(13, OUTPUT);
+  
+//  boolean notConnected = true;
+//  while(notConnected) {
+//    if((gsmAccess.begin(PINNUMBER)==GSM_READY) &
+//       (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD)==GPRS_READY))
+//      notConnected = false;
+//    else {
+//      Serial.println("Not connected");
+//      delay(1000);
+//    }
+//  }
 }
 
 void loop()
 {
-  Wire.requestFrom(I2C_ADDRESS_SLAVE, 1);
-  while (Wire.available()) {
-    char c = Wire.read();
+  if(get_gps_data()) {
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+  }
+  
+  // for debugging
+//  if (client.available())
+//  {
+//     char c = client.read();
+//     Serial.print(c);
+//  }
+
+  // if there's no net connection, but there was one last time
+  // through the loop, then stop the client:
+//  if (!client.connected() && lastConnected)
+//  {
+//    client.stop();
+//  }
+
+  // if you're not connected, and ten seconds have passed since
+  // your last connection, then connect again and send data:
+//  if(!client.connected() && ((millis() - lastConnectionTime) > postingInterval))
+//  {
+//    //sendData(sensorReading);
+//  }
+
+  // store the state of the connection for next time through the loop:
+//  lastConnected = client.connected();
+}
+
+void receive(int numBytes) { }
+
+boolean get_gps_data() {
+  if(ET.receiveData()) {
+    Serial.print(mydata.my_lat,6);
+    Serial.print(",");
+    Serial.print(mydata.my_lng,6);
+    Serial.print(",");
+    Serial.print(mydata.my_date);
+    Serial.print(",");
+    Serial.println(mydata.my_time);
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+/*
+ * Set globals: server, path, and port before calling
+ */
+boolean get() {
+  Serial.println("connecting...");
+
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    client.print("GET ");
+    client.print(path);
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(server);
+    client.println("Connection: close");
+    client.println();
+    return true;
+  } 
+  else {
+    Serial.println("connection failed");
+    return false;
+  }
+}
+
+boolean receive_response() {
+  while (client.available()) {
+    char c = client.read();
     Serial.print(c);
   }
-//  if(ET.receiveData()) {
-//    Serial.print(mydata.my_lat,6);Serial.print(",");Serial.println(mydata.my_lng,6);
-//  }
-//  char in[30];
-//  int i = 0;
-//  byte x[4];
-//  while(Wire.available())
-//  {
-//    in[i++] = Wire.read();
-//    x[i++] = Wire.read();
-//  }
-//  in[i] = '\0';
-//  Serial.println((unsigned int) x);
-  
-  delay(500);
+
+  if (!client.available() && !client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void sendData(int thisData) {
+  if (client.connect(server, 80)) {
+    Serial.println("connecting...");
+
+    // HEADER
+    client.print("PUT ");
+    client.print(path);
+    client.print("/");
+    client.print(FEEDID);
+    client.println(".csv HTTP/1.1");
+    client.println("Host: api.xively.com");
+    client.print("X-ApiKey: ");
+    client.println(APIKEY);
+    client.print("User-Agent: ");
+    client.println(USERAGENT);
+    client.print("Content-Length: ");
+
+    // 4 for "lat," + length of non-decimal part + decimal pt + 6 decimal places
+    int thisLength = 4 + getLength(floor(mydata.my_lat)) + 1 + 6;
+    thisLength += 4 + getLength(floor(mydata.my_lng)) + 1 + 6;
+    client.println(thisLength);
+
+    client.println("Content-Type: text/csv");
+    client.println("Connection: close");
+    client.println();
+
+    // BODY
+    client.print("lat,"); client.println(mydata.my_lat, 6);
+    client.print("lng,"); client.println(mydata.my_lng, 6);
+  }
+  else {
+    Serial.println("connection failed");
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+  lastConnectionTime = millis();
+}
+
+/*
+ * Calculates the number of digits in someValue
+ */
+int getLength(int someValue)
+{
+  int digits = 1;
+  int dividend = someValue /10;
+  while (dividend > 0)
+  {
+    dividend = dividend /10;
+    digits++;
+  }
+  return digits;
 }
